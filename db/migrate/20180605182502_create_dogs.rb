a4744713162de99b2acf3c4ebeca1da878f7b0e1
David Markel <davidgmarkel@gmail.com>
2018-06-05 18:26:34 +0000

    automatically backed up by learn

--- db/migrate/20180605182502_create_dogs.rb
+++ db/migrate/20180605182502_create_dogs.rb
@@ -2,12 +2,13 @@ class CreateDogs < ActiveRecord::Migration
 
   def up
     create table :dogs do |t|
-      t.string name
-      t.string breed
+      t.string :name
+      t.string :breed
     end
   end
 
   def down
+    drop table :dogs
   end
 
 end
